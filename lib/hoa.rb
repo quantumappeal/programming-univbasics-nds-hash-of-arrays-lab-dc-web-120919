12e61f3d17a5fc7351e1a378ec5d9a59f395b767
Sean Beach <sean.beach1@yahoo.com>
2019-12-06 22:49:58 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,7 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  BASE_HOA[:chimpmunks] << ["Dave"]
+  BASE_HOA[:chimpmunks] << "Dave"
   BASE_HOA[:third_earthers] << ["Snarf"]
   BASE_HOA[:jetsons] << ["Astro"]
 end
